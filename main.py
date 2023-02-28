6d2ae2424dea44cf8e5abc3b8fc8d5720b7f67fe
belalraouf <belal.raouf1@gmial.com>
2023-02-28 02:23:32 -0500

    making plot with static datas for testing.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import streamlit as st
-
+import plotly.express as px
 st.title("You can check the weather here.")
 
-location = st.text_input("location: ")
+locations = st.text_input("location: ")
 
-day = st.slider("Select time period: ", min_value=1, max_value=5)
+days = st.slider("Select time period: ", min_value=1, max_value=5)
 temperature_option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{temperature_option} or the next {day} days in {location}")
+st.subheader(f"{temperature_option} or the next {days} days in {locations}")
+
+
+def get_date(day):
+    dates = ["2023-10-03", "2023-11-13", "2023-04-20"]
+    temperatures = [12, 8, 19]
+    temperatures = [day*i for i in temperatures]
+    return dates, temperatures
+
+
+d, t = get_date(days)
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
